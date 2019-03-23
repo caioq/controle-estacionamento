0d7dbfdd44c1ed5d063b4420a2b894aae06efeac
Caio Queiroz <caiogqueiroz@gmail.com>
2019-03-23 14:35:39 -0300

    finalizacao da modelagem

---
+++ src/main/java/challenge/Cnh.java
@@ -0,0 +1,5 @@
+package challenge;
+
+public class Cnh {
+    private Integer pontos;
+}
--- src/main/java/challenge/Estacionamento.java
+++ src/main/java/challenge/Estacionamento.java
@@ -1,10 +1,13 @@
 package challenge;
 
+import java.util.LinkedHashSet;
 import java.util.List;
+import java.util.Set;
 
 public class Estacionamento {
 
     private final Integer VAGAS = 10;
 
-    private List<Carro> listCarros;
+    // Set collection - nao havera carros duplicados e ordenacao importa
+    private LinkedHashSet<Carro> carros = new LinkedHashSet<>();
 }
