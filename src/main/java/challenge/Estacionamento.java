8c3cce10146d598541bc79a871d5795da3949cd0
Caio Queiroz <caiogqueiroz@gmail.com>
2019-03-23 16:35:33 -0300

    criacao das regras de estacionar

--- src/main/java/challenge/Estacionamento.java
+++ src/main/java/challenge/Estacionamento.java
@@ -1,8 +1,9 @@
 package challenge;
 
+import challenge.exceptions.EstacionamentoException;
+
+import java.util.Iterator;
 import java.util.LinkedHashSet;
-import java.util.List;
-import java.util.Set;
 
 public class Estacionamento {
 
@@ -21,6 +22,18 @@ public class Estacionamento {
 
     // estacionar
     public void estacionar(Carro carro) {
+        // verifica se estacionamento esta cheio
+        if (carros.size() >= getVAGAS()) {
+            Iterator<Carro> itCarros = carros.iterator();
+
+            while(itCarros.hasNext()){
+                // verifica se primeiro carro a sair eh senior
+                Motorista motor = itCarros.next().getMotorista();
+                //if
+            }
+            //carros.iterator().remove();
+            //throw new EstacionamentoException("Estacionamento cheio");
+        }
         carros.add(carro);
     }
 
@@ -34,4 +47,19 @@ public class Estacionamento {
         return carros.size();
     }
 
+    public static void main(String[] args) {
+        Motorista motorista = Motorista.builder().withHabilitacao("CNH1").withIdade(24).withNome("Motor 1").withPontos(15).build();
+        Motorista motorista2 = Motorista.builder().withHabilitacao("CNH2").withIdade(60).withNome("Motor 2").withPontos(21).build();
+        Carro carro1 = Carro.builder().withCor(Cor.BRANCO).withPlaca("123").withMotorista(motorista).build();
+        Carro carro2 = Carro.builder().withCor(Cor.BRANCO).withPlaca("1234").withMotorista(motorista2).build();
+
+        LinkedHashSet<Carro> carros = new LinkedHashSet<>();
+        carros.add(carro1);
+        carros.add(carro2);
+
+        Iterator<Carro> it = carros.iterator();
+        while (it.hasNext())
+            System.out.println(it.next().getPlaca());
+
+    }
 }
