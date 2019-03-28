d909185d72e7c9312386bcc21bffd4cae4e177c0
Caio Queiroz <caiogqueiroz@gmail.com>
2019-03-27 22:37:10 -0300

    builder como inner class

--- src/main/java/challenge/Carro.java
+++ src/main/java/challenge/Carro.java
@@ -8,7 +8,7 @@ public class Carro {
 
     private Cor cor;
 
-    public Carro(Motorista motorista, String placa, Cor cor) {
+    private Carro(Motorista motorista, String placa, Cor cor) {
         this.motorista = motorista;
         this.placa = placa;
         this.cor = cor;
@@ -28,5 +28,44 @@ public class Carro {
 
     public static CarroBuilder builder() {
         return new CarroBuilder();
+    }
+
+    public static class CarroBuilder {
+        private Motorista motorista;
+
+        private String placa;
+
+        private Cor cor;
+
+        public CarroBuilder withMotorista(Motorista motorista) {
+            this.motorista = motorista;
+            return this;
+        }
+
+        public CarroBuilder withPlaca(String placa) {
+            this.placa = placa;
+            return this;
+        }
+
+        public CarroBuilder withCor(Cor cor) {
+            this.cor = cor;
+            return this;
+        }
+
+        public Carro build() {
+            if (placa == null) {
+                throw new NullPointerException("A placa é obrigatório");
+            }
+
+            if (cor == null) {
+                throw new NullPointerException("A cor é obrigatório");
+            }
+
+            if (motorista == null) {
+                throw new EstacionamentoException("O motorista é obrigatório");
+            }
+
+            return new Carro(motorista, placa, cor);
+        }
     }
 }
--- src/main/java/challenge/Cnh.java
+++
@@ -1,5 +0,0 @@
-package challenge;
-
-public class Cnh {
-    private Integer pontos;
-}
