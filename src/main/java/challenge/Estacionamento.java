e141b2f03d4ff65c1b854100d3d5a3776c78c475
Caio Queiroz <caiogqueiroz@gmail.com>
2019-03-27 22:29:37 -0300

    melhor validacao do motorista

--- src/main/java/challenge/Estacionamento.java
+++ src/main/java/challenge/Estacionamento.java
@@ -24,35 +24,40 @@ public class Estacionamento {
 
     // estacionar
     public void estacionar(Carro carro) {
+        // valida obrigacoes minimas para estacionar
+        if (carro.getMotorista().getIdade() < 18) {
+            throw new EstacionamentoException("Motorista é menor de idade.");
+        }
+        if (carro.getMotorista().getPontos() > 20) {
+            throw new EstacionamentoException("Motorista com mais de 20 pontos.");
+        }
         // verifica se estacionamento esta cheio
         if (carros.size() >= getVAGAS()) {
             Iterator<Carro> itCarros = carros.iterator();
 
-            while(itCarros.hasNext()){
+            while (itCarros.hasNext()) {
                 // verifica se primeiro carro a sair eh senior
                 Carro carroEstacionado = itCarros.next();
                 Motorista motor = carroEstacionado.getMotorista();
-                if(motor.getIdade() < 55){
-                    if(validarMotorista(motor)){
-                        carros.remove(carroEstacionado);
-                        carros.add(carro);
-                        return;
-                    }
+                if (motor.getIdade() < 55) {
+
+                    carros.remove(carroEstacionado);
+                    carros.add(carro);
+                    return;
+
                 }
             }
             // estacionamento cheio
             throw new EstacionamentoException("Estacionamento cheio");
         }
-        if(validarMotorista(carro.getMotorista())){
-            carros.add(carro);
-        }
+        carros.add(carro);
     }
 
     // carro estacionado
     public Boolean carroEstacionado(Carro carro) {
         Iterator<Carro> itCarros = carros.iterator();
-        while(itCarros.hasNext()){
-            if(itCarros.next() == carro) {
+        while (itCarros.hasNext()) {
+            if (itCarros.next() == carro) {
                 return true;
             }
         }
@@ -64,62 +69,4 @@ public class Estacionamento {
         return carros.size();
     }
 
-    public Boolean validarMotorista(Motorista motorista) {
-        if(motorista.getIdade() < 18){
-            throw new EstacionamentoException("Motorista é menor de idade.");
-        }
-        if(motorista.getPontos() > 20) {
-            throw new EstacionamentoException("Motorista com mais de 20 pontos.");
-        }
-
-        return true;
-    }
-
-    public static void main(String[] args) {
-//        Motorista motorista = Motorista.builder().withHabilitacao("CNH1").withIdade(24).withNome("Motor 1").withPontos(15).build();
-//        Motorista motorista2 = Motorista.builder().withHabilitacao("CNH2").withIdade(60).withNome("Motor 2").withPontos(21).build();
-//        Carro carro1 = Carro.builder().withCor(Cor.BRANCO).withPlaca("123").withMotorista(motorista).build();
-//        Carro carro2 = Carro.builder().withCor(Cor.BRANCO).withPlaca("1234").withMotorista(motorista2).build();
-//
-          //LinkedHashSet<Carro> carros = new LinkedHashSet<>();
-//        carros.add(carro1);
-//        carros.add(carro2);
-
-        Estacionamento estacionamento = new Estacionamento();
-
-        Motorista ada = Motorista.builder().withNome("Ada").withIdade(20)
-                .withPontos(3)
-                .withHabilitacao("11111")
-                .build();
-
-        Carro carroBranco = Carro.builder().withCor(Cor.BRANCO).
-                withPlaca("11111")
-                .withMotorista(ada)
-                .build();
-
-        estacionamento.estacionar(carroBranco);
-
-        for (int indice = 1; indice <= 10; indice++) {
-
-            Motorista motorista = Motorista.builder()
-                    .withNome("Motorista " + indice)
-                    .withIdade(20)
-                    .withPontos(3)
-                    .withHabilitacao(Long.toString(current().nextLong()))
-                    .build();
-
-            Carro carro = Carro.builder().withCor(Cor.BRANCO).
-                    withPlaca(indice + "123")
-                    .withMotorista(motorista)
-                    .build();
-
-            estacionamento.estacionar(carro);
-        }
-
-
-        Iterator<Carro> it = estacionamento.carros.iterator();
-        while (it.hasNext())
-            System.out.println(it.next().getPlaca());
-
-    }
 }
